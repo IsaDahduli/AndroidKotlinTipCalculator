a747e5600de0e44f3fc2a580b530eae597b0f8ab
Isa Dahduli <isa95dev@hotmail.com>
2024-05-30 12:41:11 +0300

    Initial commit

--- app/src/main/java/com/isa/androidkotlintipcalculator/MainActivity.kt
+++ app/src/main/java/com/isa/androidkotlintipcalculator/MainActivity.kt
@@ -8,8 +8,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
